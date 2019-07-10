afe8644034708648a2f276649b97605ca4b8e1ac
Michael T. Kelbaugh <Michael.Kelbaugh@jhuapl.edu>
2019-07-10 13:59:47 -0400

    intial connectivity between Armington trade model and civil conflict model
    Former-commit-id: 9f3be1168d5f6eae50f406a1cd0c30c1d721595b

---
+++ models/examples/armington/src/inner_wrapper.py
@@ -0,0 +1,86 @@
+import glob
+import os
+import sys
+sys.path.append('/')
+from outer_wrapper import OuterWrapper
+from armington import ArmingtonTrade
+
+import pymongo
+import gridfs
+MONGO_IP = os.environ.get('MONGO_IP', "mongodb:27017")
+client = pymongo.MongoClient("mongodb://{}/".format(MONGO_IP))
+status = client.server_info()
+print(status)
+files_db = client["files"]
+fs = gridfs.GridFS(files_db)
+
+from bokeh.plotting import figure
+from bokeh.embed import file_html
+from bokeh.resources import CDN
+from bokeh.layouts import column
+colors = ["red", "green", "blue"]
+labels = ["wheat", "rice", "cassava"]
+TOOLS = "pan, box_zoom, wheel_zoom, undo, redo, reset, hover, save"
+
+TOOLTIPS = [
+("price", "$y"),
+("year", "$x{int}"),
+]
+
+class InnerWrapper(OuterWrapper):
+
+    def __init__(self):
+        num_input_schemas = len(glob.glob("/opt/schemas/input/*.json"))
+        super().__init__(model_id="armington", num_expected_inputs=num_input_schemas)
+        self.elasticity = None
+        self.initial_prices = None
+        self.initial_consumption = None
+
+        self.initial_year = 2020
+
+        self.new_prices_series = [ [1, 2, 3], [2, 4, 5], [1, 2, 3] , [1, 1, 1], [5, 4, 1], [10, 8, 2], [15, 10, 2], [2, 2, 2], [4, 4, 4], [3, 6, 9] ]
+        self.new_consumption_series = []
+        self.delta_series = []
+
+    def configure(self, **kwargs):
+        self.elasticity = kwargs['config']['elasticity']
+        self.initial_prices = kwargs['config']['initial_prices']
+        self.initial_consumption = kwargs['config']['initial_consumption']
+        self.trade = ArmingtonTrade(self.elasticity, self.initial_consumption, self.initial_prices)
+
+    def increment(self, **kwargs):
+        print(kwargs.keys())
+        consumption, delta = self.trade.run(self.new_prices_series[self.incstep])
+        self.new_consumption_series.append(consumption)
+        self.delta_series.append(delta)
+        consumption_dict = {}
+        for i in range(len(consumption)):
+            consumption_dict["crop{}".format(i)] = consumption[i]
+
+        # prices
+        prices_plot = figure(title="Prices", plot_width=400, plot_height=400, tools=TOOLS, tooltips=TOOLTIPS)
+        for i in range(3):
+            prices = [series[i] for series in self.new_prices_series[:len(self.new_consumption_series)]]
+            prices_plot.line(range(self.initial_year, self.initial_year + len(self.new_prices_series)), prices, line_width=2, legend=labels[i], line_color=colors[i])
+
+        # consumption
+        consumption_plot = figure(title="Consumption (elasticity = {})".format(self.elasticity), plot_width=400, plot_height=400, tools=TOOLS, tooltips=TOOLTIPS)
+        for i in range(3):
+            consumption = [series[i] for series in self.new_consumption_series]
+            consumption_plot.line(range(self.initial_year, self.initial_year + len(self.new_prices_series)), consumption, line_width=2, legend=labels[i], line_color=colors[i])
+
+        # save file in Mongo database
+        html = file_html(column(prices_plot, consumption_plot), CDN, title="armington")
+        html = html.encode()
+        fs.put(html, filename="armington_inc{}.html".format(self.incstep))
+ 
+        return {'armington': {'consumption': {'data': consumption_dict, 'granularity': 'national'}, 'gdp_delta': {'data': {'gdp_delta': delta, 'year_actual': self.initial_year + self.incstep, 'ccode': '500'}, 'granularity': 'national'}}}
+
+
+def main():
+    wrapper = InnerWrapper()
+    wrapper.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/examples/civil_conflict/src/inner_wrapper.py
@@ -0,0 +1,55 @@
+import glob
+import os
+import sys
+sys.path.append('/')
+from outer_wrapper import OuterWrapper
+from CivilConflict import CivilConflict
+
+import pymongo
+import gridfs
+MONGO_IP = os.environ.get('MONGO_IP', "mongodb:27017")
+client = pymongo.MongoClient("mongodb://{}/".format(MONGO_IP))
+status = client.server_info()
+print(status)
+files_db = client["files"]
+fs = gridfs.GridFS(files_db)
+
+class InnerWrapper(OuterWrapper):
+
+    def __init__(self):
+        num_input_schemas = len(glob.glob("/opt/schemas/input/*.json"))
+        print("num input schemas " + str(num_input_schemas))
+        super().__init__(model_id="civil_conflict", num_expected_inputs=num_input_schemas)
+        self.data = None
+        self.gdps = []
+        self.years = []
+
+    def configure(self, **kwargs):
+        self.gdp = kwargs['config']['gdp_delta']
+        self.year_actual = kwargs['config']['year_actual']
+        self.ccode = kwargs['config']['ccode']
+
+    def increment(self, **kwargs):
+        print(kwargs.keys())
+        if 'armington' in kwargs.keys():
+            self.gdp = kwargs['armington']['gdp_delta']['data']['gdp_delta']
+            self.year_actual = kwargs['armington']['gdp_delta']['data']['year_actual']
+            self.ccode = kwargs['armington']['gdp_delta']['data']['ccode']
+        else:
+            print("armington input data not found")
+
+        self.gdps.append(self.gdp)
+        self.years.append(self.year_actual)
+        conflict = CivilConflict()
+        html = conflict.run(self.gdps, self.years)
+        fs.put(html, filename="civil_conflict_inc{}.html".format(self.incstep))
+
+        return {'civil_conflict': {'data_variable_name': {'data': {}, 'granularity': 'county'}}}
+
+def main():
+    wrapper = InnerWrapper()
+    wrapper.run()
+
+
+if __name__ == "__main__":
+    main()
