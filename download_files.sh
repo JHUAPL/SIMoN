a479d5307024f559bcabcc228b0fac50b7910f31
Michael T. Kelbaugh <Michael.Kelbaugh@jhuapl.edu>
2019-07-15 10:26:08 -0400

    script to download visualizations from the database
    Former-commit-id: 8a3a10a0d0e002e4ada353c166543918b1c1e39c

--- download_files.sh
+++ download_files.sh
@@ -2,4 +2,6 @@
 
 mkdir files
 cd files
-docker exec mongodb mongofiles --db files list | awk '{print $1}' | xargs -I {} docker exec mongodb mongofiles --db files get {} && docker exec mongodb mongofiles --db files list | awk '{print $1}' | xargs -I {} docker cp mongodb:/{} .
+docker exec mongodb mongofiles --db files list | awk '{print $1}' | xargs -I {} docker exec mongodb mongofiles --db files get {} \
+&& docker exec mongodb mongofiles --db files list | awk '{print $1}' | xargs -I {} docker cp mongodb:/{} .
+
