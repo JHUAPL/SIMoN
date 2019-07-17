3d2ec00406ba16958c0e54d704af2c4a0ef063b7
Alex Fiallos <afiallo1@jhu.edu>
2019-07-17 15:52:16 -0400

    Changed docker-compose to run mongodb container
    Former-commit-id: 5bda7a792fb6a6bd4859e59cd7046bda5bebbce2

--- models/examples/rcp_climate/src/climate_model.py
+++ models/examples/rcp_climate/src/climate_model.py
@@ -15,11 +15,11 @@ def temp_inc(init_data, year):
     for i in json1_data:
         #print(json1_data)
         for j in json1_data[i]:
-            mean_glob_temps.append(json1_data[i][j][year][2])
+            mean_glob_temps.append(json1_data[i][j][year][2]-273.15)
     return np.mean(mean_glob_temps)
 
 # with open("D:/STW_Models/simon/models/examples/rcp_climate/config/rcp26data.json", 'r') as file:
 #     test = json.load(file)
 #
-# print(temp_inc(test, 1))
+# print(temp_inc(test, 94))
 
