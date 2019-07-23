8ef466ca4550aa008a88e29fe530ff01905cc208
Alex Fiallos <afiallo1@jhu.edu>
2019-07-23 16:05:20 -0400

    Bug fix: corrected global mean temp
    Former-commit-id: 82f5315cf98864495938a4bb201ce1996ec00ac2

--- models/examples/rcp_climate/src/inner_wrapper.py
+++ models/examples/rcp_climate/src/inner_wrapper.py
@@ -11,8 +11,6 @@ class InnerWrapper(OuterWrapper):
         super().__init__(model_id="rcp_climate", num_expected_inputs=num_input_schemas)
 
     def configure(self, **kwargs):
-        # self.data = kwargs['rcp26data']
-        print(self.incstep)
         self.raw_data = kwargs['rcp26data']
         if 'rcp26data' in kwargs.keys():
             self.data = temp_inc(self.raw_data, self.incstep)
@@ -24,15 +22,7 @@ class InnerWrapper(OuterWrapper):
             print('rcp data not found')
 
     def increment(self, **kwargs):
-        # print(self.incstep)
-        #
-        # if self.incstep != 94:
-        #     if 'rcp26data' in kwargs.keys():
         self.data = temp_inc(self.raw_data, self.incstep)
-            # self.configure(self,**kwargs)
-        # else:
-        #     if 'rcp26data' in kwargs.keys():
-        #         self.data = temp_inc(kwargs['rcp26data'], 94)
 
         return {'rcp_climate': {'rcp_climate': {'data': {'temp': self.data}, 'granularity': 'global'}}}
 
