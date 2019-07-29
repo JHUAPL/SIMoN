9b89ce7f6c7dc1aa6a2f4a894a3507d242308c73
Alex Fiallos <afiallo1@jhu.edu>
2019-07-29 11:46:32 -0400

    Added surface water runoff(mm) to model
    Former-commit-id: 875543cd6403320402d5e4b80aeddfdbe41a10fa

--- models/examples/rcp_climate/src/climate_model.py
+++ models/examples/rcp_climate/src/climate_model.py
@@ -19,25 +19,32 @@ def temp_inc(init_data, year):
 
     for i in json1_data:
         if 49 >= float(i) >= 24:
-            # Convert to format that plays nice with mongodb
+            # Convert to format that plays nice with mongodb; only get U.S squares
             single_year_US[i.replace('.', '_')] = {}
         for j in json1_data[i]:
             # Use two outputs, single global mean temperature and gridded climate data
+            mean_glob_temps.append(json1_data[i][j][year][3])
+
             # Contiguous U.S bounded by (49 N, 122W), (24N 66W)
             if 49 >= float(i) >= 24 and -65 >= float(j) >= -122:
                 single_year_US[i.replace('.', '_')][j.replace('.', '_')] = (
-                    json1_data[i][j][year][0], json1_data[i][j][year][1], json1_data[i][j][year][2]-273.15)
+                    json1_data[i][j][year][0], json1_data[i][j][year][1], json1_data[i][j][year][2],
+                    json1_data[i][j][year][3]-273.15
+                )
 
-            mean_glob_temps.append(json1_data[i][j][year][2])
     # Apply weights
     weighted_sum = np.sum([a*b for a, b in zip(mean_glob_temps, weights)])
-    # Output: Global average (C) + grid of U.S (precipitation(mm), evapo-transpiration (mm), surface temp(C))
+    # Output: Global average (C) +
+    # grid of U.S (precipitation (mm), evaporation (mm), surface runoff (mm), surface temp(C))
     return weighted_sum-273.15, single_year_US
 
-# test
-
-
+# # test
+#
+#
 # with open("D:/STW_Models/simon/models/examples/rcp_climate/config/rcp26data.json", 'r') as file:
 #     test = json.load(file)
 # v1, v2 = temp_inc(test, 0)
 # print(v2)
+
+# with open('climate_data_year0.json', 'w') as outf:
+#     json.dump(v2, outf)
