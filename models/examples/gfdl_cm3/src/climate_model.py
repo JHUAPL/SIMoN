cb0807c8abfb53155fb1b159a51e7623b70f3571
AF1111 <afiallo1@jhu.edu>
2020-02-11 17:57:47 -0500

    fixing indexing issue
    Former-commit-id: fddfff2225769cd11123eb277e3e3c58ce5943d1

--- models/examples/gfdl_cm3/src/climate_model.py
+++ models/examples/gfdl_cm3/src/climate_model.py
@@ -10,10 +10,6 @@ import json
 
 def temp_inc(init_data, year):
     json1_data = init_data
-<<<<<<< HEAD
-=======
-    year = year - 1
->>>>>>> 154bcdf2bb3d01cda303fe3e9cbbe67a926c0763
     mean_glob_temps = []
     with open("/opt/src/weights.json") as f:
         weights = json.load(f)
@@ -32,13 +28,9 @@ def temp_inc(init_data, year):
             # Contiguous U.S bounded by (49 N, 122W), (24N 66W)
             if 49 >= float(i) >= 23 and -68 >= float(j) >= -128:
                 single_year_US[i][j] = (
-<<<<<<< HEAD
-                    json1_data[i][j][year][0], json1_data[i][j][year][1], json1_data[i][j][year][3]-273.15
-=======
                     json1_data[i][j][year][0],
                     json1_data[i][j][year][1],
-                    json1_data[i][j][year][2] - 273.15,
->>>>>>> 154bcdf2bb3d01cda303fe3e9cbbe67a926c0763
+                    json1_data[i][j][year][3] - 273.15,
                 )
 
     # Apply weights
@@ -54,12 +46,8 @@ def temp_inc(init_data, year):
             lon = float(lon)
             if lon < 0:
                 lon += 180
-            translated_pr[
-                f"lat_{int(lat*100)}_lon_{int(lon*100)}"
-            ] = lon_values[0]
-            translated_ev[
-                f"lat_{int(lat*100)}_lon_{int(lon*100)}"
-            ] = lon_values[1]
+            translated_pr[f"lat_{int(lat*100)}_lon_{int(lon*100)}"] = lon_values[0]
+            translated_ev[f"lat_{int(lat*100)}_lon_{int(lon*100)}"] = lon_values[1]
 
     return (
         weighted_sum - 273.15,
