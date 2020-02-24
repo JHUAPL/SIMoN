d547d42920502c41930465dbe9a0d3cc9714e317
Michael T. Kelbaugh <Michael.Kelbaugh@jhuapl.edu>
2020-02-24 16:34:18 -0500

    issue #5 query must be in extended json v2 format for MongoDB >= 4.2
    Former-commit-id: cb51db95fd2294d738f5c71098b93b32fe17cdce

--- viz/export.sh
+++ viz/export.sh
@@ -11,7 +11,7 @@ docker start "simon_mongodb"
 # retrieve the document from the database and save it as a JSON file
 docker exec -it $DB_CONTAINER \
   mongoexport --db broker --collection sub --limit 1 \
-    --query "{source: \"$MODEL_NAME\", year: $YEAR}" \
+    --query "{\"source\": \"$MODEL_NAME\", \"year\": $YEAR}" \
     --out $JSON_DATA
 
 # copy the JSON file from the container to the host
